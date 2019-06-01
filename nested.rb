560f593af50b1d4848358b69f6362e52803408fe
Jack de Grave <jack.degrave@outlook.com>
2019-06-01 13:32:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages][0] << "Assembly"
+programmer_hash[:dennis_ritchie][:languages][0]
+return programmer_hash << 
 end
